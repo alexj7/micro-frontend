2958d7e256a66481fb7a2cb3598e45ba6236c512
Alexander Labrador <alexlador2@gmail.com>
2025-03-13 12:32:32 -0400

    chore(conifg): update env config and clean trash files

--- micro-host-app/src/vite-env.d.ts
+++ micro-host-app/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_PUBLIC_URL: string;
+    readonly VITE_MICRO_1_URL: string;
+    readonly VITE_MICRO_2_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
--- micro-host-app/vite.config.ts
+++ micro-host-app/vite.config.ts
@@ -1,33 +1,37 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import federation from '@originjs/vite-plugin-federation';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'host-app',
-      remotes: {
-        'micro-character-list-1': 'http://localhost:3001/assets/remoteEntry.js',
-        'micro-character-list-2': 'http://localhost:3002/assets/remoteEntry.js',
-      },
-      shared: ['react', 'react-dom', 'react-i18next', 'i18next'],
-    }),
-  ],
-  build: {
-    target: 'esnext',
-    modulePreload: false,
-    minify: false,
-    cssCodeSplit: false,
-  },
-  server: {
-    cors: true,
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    reporters: ['verbose'],
-    setupFiles: './src/setupTests.ts',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: 'host-app',
+        remotes: {
+          'micro-character-list-1': `${env.VITE_MICRO_1_URL}/assets/remoteEntry.js`,
+          'micro-character-list-2': `${env.VITE_MICRO_2_URL}/assets/remoteEntry.js`,
+        },
+        shared: ['react', 'react-dom', 'react-i18next', 'i18next'],
+      }),
+    ],
+    build: {
+      target: 'esnext',
+      modulePreload: false,
+      minify: false,
+      cssCodeSplit: false,
+    },
+    server: {
+      cors: true,
+      port: 3000,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      reporters: ['verbose'],
+      setupFiles: './src/setupTests.ts',
+    }
+  }
 });
